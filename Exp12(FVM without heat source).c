c60639f4c01e77baec30e6f2e8ddb4450572a9cf
ananya2001k <155174254+ananya2001k@users.noreply.github.com>
2024-11-11 08:32:59 +0530

    Add files via upload

---
+++ Exp12(FVM without heat source).c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    float L,Ta,Tb,k,h,B,A,alpha[10],beta[10],a[10],b[10],c[10],d[10],x[10];
+    printf("Enter the length of the rod:");
+    scanf("%f",&L);
+    printf("Enter the number of nodal points:");
+    scanf("%d",&n);
+    printf("Enter the temperature at the left end of the rod:");
+    scanf("%f",&Ta);
+    printf("Enter the temperature at the right end of the rod:");
+    scanf("%f",&Tb);
+    printf("Enter the value of cross section area:");
+    scanf("%f",&A);
+    printf("Enter the thermal conductivity:");
+    scanf("%f",&k);
+    h=L/n;
+    B=(k*A)/h;
+    for(i=2;i<=n;i++)
+        a[i]=-B;
+    for(i=1;i<n;i++)
+        c[i]=-B;
+    for(i=1;i<=n;i++)
+    {
+        if(i==1 || i==n)
+            b[i]=3*B;
+        else
+            b[i]=2*B;
+    }
+    for(i=2;i<n;i++)
+            d[i]=0;
+    d[1]=2*Ta*B;
+    d[n]=2*Tb*B;
+    alpha[1]=b[1];
+    beta[1]=d[1]/b[1];
+    for(i=2;i<=n;i++)
+    {
+        alpha[i]=b[i]-((a[i]*c[i-1])/alpha[i-1]);
+        beta[i]=(d[i]-a[i]*beta[i-1])/alpha[i];
+    }
+    x[n]=beta[n];
+    for(i=n-1;i>0;i--)
+        x[i]=beta[i]-((c[i]*x[i+1])/alpha[i]);
+    printf("the solution of the given system is:\n");
+    for(i=1;i<=n;i++)
+        printf("%0.3f\t",x[i]);
+}
